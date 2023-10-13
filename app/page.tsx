d1ac10078017767eea0c9540795a7933846610c4
sarorsaad <sarorsaad@gmail.com>
2023-10-13 21:38:50 +0300

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import styles from '@/styles/home.module.css'
+
+const page = () => {
+  return (
+    <div className={styles['home']}>
+      <h1> this is home page</h1>
+    </div>
+  )
+}
+
+export default page
